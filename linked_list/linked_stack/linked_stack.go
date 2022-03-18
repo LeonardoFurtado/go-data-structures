cfdae9631c60b1f335b4cdbc71358a2c4c08363d
Leonardo Furtado <srleonardofurtado@gmail.com>
2022-03-17 22:47:06 -0300

    feat: add push method to linkedStack.

--- linked_list/linked_stack/linked_stack.go
+++ linked_list/linked_stack/linked_stack.go
@@ -1,1 +1,17 @@
 package linked_stack
+
+type Node struct {
+	element int
+	next    *Node
+}
+
+type LinkedStack struct {
+	head *Node
+	size int
+}
+
+func (l *LinkedStack) Push(value int) {
+	newNode := Node{value, l.head}
+	l.head = &newNode
+	l.size++
+}
--- linked_list/linked_stack/linked_stack_test.go
+++ linked_list/linked_stack/linked_stack_test.go
@@ -1,1 +1,15 @@
 package linked_stack
+
+import (
+	"testing"
+)
+
+func TestPush(t *testing.T) {
+	n3 := Node{10, nil}
+	n2 := Node{20, &n3}
+	stack := LinkedStack{&n2, 2}
+	stack.Push(30)
+	if stack.head.element != 30 {
+		t.Errorf("Value was not inserted on top.")
+	}
+}
