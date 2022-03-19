2782da2ce358771b491ccb6aa3a81472f6837779
Leonardo Furtado <srleonardofurtado@gmail.com>
2022-03-18 23:20:24 -0300

    feat: add linked queue Enqueue and IsEmpty method.

--- linked_list/linked_queue/linked_queue.go
+++ linked_list/linked_queue/linked_queue.go
@@ -1,1 +1,33 @@
 package linked_queue
+
+/*
+FIFO queue implementation using a singly linked list for storage.
+*/
+
+type Node struct {
+	element int
+	next    *Node
+}
+
+type LinkedQueue struct {
+	head *Node
+	tail *Node
+	size int
+}
+
+//Enqueue add an element to the back of queue.
+func (l *LinkedQueue) Enqueue(element int) {
+	newest := &Node{element, nil}
+	if l.IsEmpty() {
+		l.head = newest
+	} else {
+		l.tail.next = newest
+	}
+	l.tail = newest
+	l.size++
+}
+
+//IsEmpty return true if the queue is empty.
+func (l *LinkedQueue) IsEmpty() bool {
+	return l.size == 0
+}
--- linked_list/linked_queue/linked_queue_test.go
+++ linked_list/linked_queue/linked_queue_test.go
@@ -1,1 +1,22 @@
 package linked_queue
+
+import "testing"
+
+func TestEnqueue(t *testing.T) {
+	n3 := Node{10, nil}
+	n2 := Node{10, &n3}
+	n1 := Node{10, &n2}
+	queue := LinkedQueue{&n1, &n3, 3}
+	queue.Enqueue(40)
+	if queue.tail.element != 40 {
+		t.Errorf("Value was not enqueued")
+	}
+}
+
+func TestIsEmpty(t *testing.T) {
+	queue := LinkedQueue{}
+	got := queue.IsEmpty()
+	if got != true {
+		t.Errorf("got false but want true")
+	}
+}
